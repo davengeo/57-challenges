e6105db3814f9ffd860c3b0b4ac33d5de0d8c66c
David Navarro Alvarez <davengeo@yahoo.es>
2015-08-05 19:17:05 +0200

    with different greetings per input

--- 1.saying-hello/src/saying_hello.clj
+++ 1.saying-hello/src/saying_hello.clj
@@ -7,21 +7,21 @@
 
 ;without vars
 (do
-  (println "What's your name")
+  (println "What's your name?")
   (println "Hello, " (read-line) ". Nice to meet you!")
  )
 
 ;different greetings
 (do
-  (println "What's your name")
+  (println "What's your name?")
   (let [my-input (read-line) greeting nil]
-    (if (re-matches #"David" my-input)
+    (when (re-find #"David" my-input)
       (def greeting "Hola, ")
     )
-    (if (re-matches #"Peter" my-input)
+    (when (re-find #"Peter" my-input)
       (def greeting "Hello, ")
     )
-    (if-not greeting
+    (when-not greeting
       (def greeting "Stranger, ")
     )
     (println greeting my-input ". Nice to meet you!")
