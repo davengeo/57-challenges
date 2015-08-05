f45d2aaca74d64b30d340144a02f553e55279c78
David Navarro Alvarez <davengeo@yahoo.es>
2015-08-05 18:43:01 +0200

    without var solution

--- 1.saying-hello/src/saying_hello.clj
+++ 1.saying-hello/src/saying_hello.clj
@@ -1,5 +1,15 @@
 (ns saying-hello)
 
+;with vars
 (println "What's your name?")
 (let [my-input (read-line)]
-     (println "Hello, " my-input ". Nice to meet you!David"))
+     (println "Hello, " my-input ". Nice to meet you!"))
+
+;without vars
+(do
+  (println "What's your name")
+  (print "Hello, " (read-line) ". Nice to meet you!")
+ )
+
+;different greetings
+
