5c0e39ffaea731b2e520990a952f0adfc2c439d4
David Navarro Alvarez <davengeo@yahoo.es>
2015-08-06 19:17:34 +0200

    with dictionaries solution for greetings

---
+++ 1.saying-hello/src/greeting_hello.clj
@@ -0,0 +1,23 @@
+(ns greeting-hello)
+
+;globals
+(def greetings {:spanish "Hola"
+								:english "Hello"})
+(def surnames {:spanish '(David, Pedro)
+							 :english '(Alistair, Peter)})
+
+(defn print-greeting [greeting my-input]
+	(println greeting my-input ".This is a test!"))
+
+(do
+	(println "What's your name?")
+	(def greeting "Stranger")
+	(let [my-input (read-line)]
+		(doseq [[k v] surnames]
+			(doseq [iter v]
+				(if (.contains (re-find #"(\w+)" my-input) (str iter))
+					(with-redefs [greeting (k greetings)]
+						(print-greeting greeting my-input)
+))))))
+
+;TODO: use an atom for filter the if
