d0e3d9de16fb6ced88435b5f6cc11ff9d8711cf4
David Navarro Alvarez <davengeo@yahoo.es>
2015-08-07 21:50:53 +0200

    solved

--- 5.simple-math/src/simple_math.clj
+++ 5.simple-math/src/simple_math.clj
@@ -1,11 +1,8 @@
 (ns simple-math)
 
-(defn to-int [chr]
-	(bigint chr))
-
 (defn read-int [pos]
 	(println "What is the" pos "number?")
-	to-int (read-line))
+	(bigint (read-line)))
 
 (let [first-number (read-int "first")]
 	(let [second-number (read-int "second")]
