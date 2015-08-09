3dee1508c7261fc9e93dc150c014bf8960a8612d
David Navarro Alvarez <davengeo@yahoo.es>
2015-08-09 23:26:27 +0300

    solved

--- 5.simple-math/src/simple_math.clj
+++ 5.simple-math/src/simple_math.clj
@@ -4,11 +4,9 @@
 	(println "What is the" pos "number?")
 	(bigint (read-line)))
 
-(def oper [+ - * /])
+(def oper [{:p + :n "+"} {:p - :n "-"} {:p * :n "x"} {:p / :n "/"}])
 
 (let [first-number (read-int "first")]
 	(let [second-number (read-int "second")]
 		(doseq [op oper]
-			(println (str first-number op second-number "=" (op first-number second-number))))))
-
-;how can I get the name of the function?
+			(println (str first-number (:n op) second-number "=" ((:p op) first-number second-number))))))
