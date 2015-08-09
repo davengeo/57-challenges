5b49cf0ecac93a647ebc81606d93f50b762d5387
David Navarro Alvarez <davengeo@yahoo.es>
2015-08-09 23:31:31 +0300

    simplifying

--- 5.simple-math/src/simple_math.clj
+++ 5.simple-math/src/simple_math.clj
@@ -6,7 +6,7 @@
 
 (def oper [{:p + :n "+"} {:p - :n "-"} {:p * :n "x"} {:p / :n "/"}])
 
-(let [first-number (read-int "first")]
-	(let [second-number (read-int "second")]
+(let [first (read-int "first")]
+	(let [second (read-int "second")]
 		(doseq [op oper]
-			(println (str first-number (:n op) second-number "=" ((:p op) first-number second-number))))))
+			(println (imstr first (:n op) second "=" ((:p op) first second))))))
