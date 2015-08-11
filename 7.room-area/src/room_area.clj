340aeec05a49029e5d85f923ef51188dbea88415
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-11 16:48:18 +0300

    solved

--- 7.room-area/src/room_area.clj
+++ 7.room-area/src/room_area.clj
@@ -1,4 +1,4 @@
- (ns room-area)
+(ns room-area)
 
 (defn read-int [dim]
       (println "What is the" dim "of the room in feet?")
@@ -7,11 +7,14 @@
 (defn calc-area [length width]
   (* length width))
 
+(defn feet-meter [feet]
+  (Math/sqrt (* (* feet feet) 0.09290304)))
+
 (let  [length (read-int "length")
        width (read-int "width")]
       (println "You entered dimensions of " length  " feet by " width " feet.")
       (println "The area is")
       (println (calc-area length width) "square feet")
-      (println "ja"))
+      (println (calc-area (feet-meter length) (feet-meter width)) "square meters"))
 
 ; m2 = f2 × 0.09290304
