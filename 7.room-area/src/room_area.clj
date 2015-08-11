323f693635fb224316a04e9d5dbe00b7068c2881
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-11 16:32:42 +0300

    simple with square feet

--- 7.room-area/src/room_area.clj
+++ 7.room-area/src/room_area.clj
@@ -1,1 +1,17 @@
  (ns room-area)
+
+(defn read-int [dim]
+      (println "What is the" dim "of the room in feet?")
+      (bigint (read-line)))
+
+(defn calc-area [length width]
+  (* length width))
+
+(let  [length (read-int "length")
+       width (read-int "width")]
+      (println "You entered dimensions of " length  " feet by " width " feet.")
+      (println "The area is")
+      (println (calc-area length width) "square feet")
+      (println "ja"))
+
+; m2 = f2 × 0.09290304
