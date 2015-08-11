e6e10de2c398ff8f2f935774a1ab8e090d48faf4
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-11 11:24:05 +0300

    using java.util.Calendar api for get current year

--- 6.retirement-calc/src/retirement_calc.clj
+++ 6.retirement-calc/src/retirement_calc.clj
@@ -1,1 +1,9 @@
-(ns retirement-calc)
+(ns retirement-calc
+  (:require
+    [clojure.instant :as instant])
+  (:import (java.util Calendar)))
+(println "what is your current age?")
+(let [my-age (read-line)]
+  (prn (str (.. Calendar (getInstance) (get Calendar/YEAR)) my-age))
+
+  )
