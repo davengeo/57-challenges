c4e13e308077d022f9d5d4b2ecf949d2d044d790
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-11 18:07:51 +0300

    solved using multimethods

--- 7.room-area/src/room_area.clj
+++ 7.room-area/src/room_area.clj
@@ -4,17 +4,18 @@
       (println "What is the" dim "of the room in feet?")
       (bigint (read-line)))
 
-(defn calc-area [length width]
-  (* length width))
-
 (defn feet-meter [feet]
   (Math/sqrt (* (* feet feet) 0.09290304)))
+
+(defmulti area :type)
+(defmethod area :feet [shape]
+  (* (:length shape) (:width shape)))
+(defmethod area :meters [shape]
+  (* (feet-meter (:length shape)) (feet-meter (:width shape))))
 
 (let  [length (read-int "length")
        width (read-int "width")]
       (println "You entered dimensions of " length  " feet by " width " feet.")
       (println "The area is")
-      (println (calc-area length width) "square feet")
-      (println (calc-area (feet-meter length) (feet-meter width)) "square meters"))
-
-; m2 = f2 × 0.09290304
+      (println (area {:type :feet :length length :width width}) "square feet")
+      (println (area {:type :meters :length length :width width}) "square meters"))
