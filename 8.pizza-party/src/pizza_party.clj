da3108d513f906c724039f7935f153cdc3ff45ce
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-12 12:28:21 +0300

    stupid solution

--- 8.pizza-party/src/pizza_party.clj
+++ 8.pizza-party/src/pizza_party.clj
@@ -1,1 +1,10 @@
- (ns pizza-party)
+(ns pizza-party)
+(defn read-int [question]
+ (println question)
+ (bigint (read-line)))
+
+(let [people (read-int "How many people?")
+      pizzas (read-int "How many pizzas?")
+      slicing (read-int "How many slices per pizza?")]
+  (println "So" people "with" pizzas "pizzas...")
+  (println "It is " (/ (* pizzas slicing) people) "each person."))
