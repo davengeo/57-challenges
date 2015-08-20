519c025c8119be4bef34dce9f81384e54058580c
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-20 16:17:49 +0300

    it's working better but not round up
    Module [57-challenges]:
      + paint_calculator.clj

--- 9.paint-calculator/src/paint_calculator.clj
+++ 9.paint-calculator/src/paint_calculator.clj
@@ -1,1 +1,14 @@
  (ns paint-calculator)
+(def ^:dynamic conversion-rate 350)
+(defn read-int [dim]
+  (println "What is the" dim "of the room in feet?")
+  (bigint (read-line)))
+
+(defn paint-gallons [area]
+  (/ area conversion-rate))
+(let  [length (read-int "length")
+       width (read-int "width")
+       area (* length width)]
+  (println "so we have" area "square feet")
+  (println "you are gonna need" (paint-gallons area) "gallons of paint")
+)
