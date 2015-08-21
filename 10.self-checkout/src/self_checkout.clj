6de461e7d2f479fb8a18702bb1f254bcb3e2671c
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-21 16:38:33 +0300

    excepting the tax, using map and reduce

--- 10.self-checkout/src/self_checkout.clj
+++ 10.self-checkout/src/self_checkout.clj
@@ -1,1 +1,15 @@
  (ns self-checkout)
+
+(defn read-about [word item-num]
+  (println "What is the" word "of the item #" item-num "?")
+  (bigint (read-line)))
+
+(defn read-price [pos]
+  (* (read-about "price" pos) (read-about "quantity" pos)))
+
+(defn gross-price [coll]
+  (int (reduce + 0 (map read-price coll))))
+
+(let [total (gross-price (range 3))]
+  (println "Total is " total))
+
