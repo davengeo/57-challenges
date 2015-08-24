6011fa3176e9074bde6062ccd446beb9a051d232
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-24 15:20:30 +0300

    retry macro for read number from input
    Module [57-challenges]:
      + currency_convertion.clj

--- 11.currency-convertion/src/currency_convertion.clj
+++ 11.currency-convertion/src/currency_convertion.clj
@@ -1,7 +1,7 @@
 (ns currency-convertion)
 
 ;using the paradigm explained in https://gist.github.com/john-kurkowski/3484356
-(defn try-times-wait-millis* [times thunk]
+(defn try-times* [times thunk]
   (let [attemps (first (drop-while #{::fail}
                                    (repeatedly times
                                                #(try
@@ -9,13 +9,13 @@
                                                  (catch Exception _ ::fail)))))]
     (::sucess attemps)))
 
-(defmacro try-times-wait-millis
-  [times wait-millis & thunk]
-  `(try-times-wait-millis* ~times (fn [] ~@thunk)))
+(defmacro try-times
+  [times & thunk]
+  `(try-times* ~times (fn [] ~@thunk)))
 
 
 (defn read-number-with-question [question]
-  (try-times-wait-millis 3 (do (println question) (bigint (read-line)))))
+  (try-times 3 (do (println question) (bigint (read-line)))))
 
 (let [currency (read-number-with-question "What is the number?")]
   (prn currency))
