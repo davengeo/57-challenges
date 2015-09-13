e3f05baa92c64f24fe151c868fa87d1b9bd0d11d
David Navarro Alvarez <davengeo@yahoo.es>
2015-09-13 10:55:56 +0300

    monads skeleton

--- monads/src/monads/functors.clj
+++ monads/src/monads/functors.clj
@@ -1,1 +1,12 @@
 (ns monads.functors)
+
+(defprotocol Functor
+	(fmap [functor f] "Maps fn over the functor f"))
+
+(defrecord List [wrapped]
+	Functor
+	(fmap [functor f]
+		(List. (map f (:wrapped functor)))))
+
+(def my-list-functor (List. [1 2 3]))
+<
