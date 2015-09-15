0ba6d1e180f2a5d984de4148f8b2937834a5b694
David Navarro Alvarez <davengeo@yahoo.es>
2015-09-15 14:22:16 +0300

    functional applicatives

--- monads/src/monads/functors.clj
+++ monads/src/monads/functors.clj
@@ -17,7 +17,36 @@
 
 (fmap my-list-functor (comp f g))
 
+(defmulti pure (fn [f _] f))
 
+(defmethod pure List [_ v]
+	"Wraps value v in a list"
+	(List. [v]))
 
+(defmulti <*> (fn [fs _] (class fs)))
+
+(defmethod <*> List [fs xs]
+	"Unwraps the functions in fs,
+	applies them to the Functors in xs,
+	 wrapping the result at the end"
+	(List. (for [f (:wrapped fs)
+							 x (:wrapped xs)]
+					 (f x))))
+
+(defmethod <*> List [fs xs]
+	"Unwraps the functions in fs,
+	applies them to the Functors in xs,
+	wrapping the result at the end"
+	(List. (for [f (:wrapped fs)
+							 x (:wrapped xs)]
+					 (f x))))
+
+(def fs (List. [#(* 2 %) #(+ 10 %)]))
+
+(def xs (List. [1 2 3]))
+
+(def u (List. [#(* 2 %)]))
+(def v (List. [#(+ 10 %)]))
+(def w (List. [1 2 3]))
 
 
--- monads/test/monads/functors_test.clj
+++ monads/test/monads/functors_test.clj
@@ -4,6 +4,8 @@
 	(:use midje.sweet)
 	(:import (monads.functors List)))
 
+;http://www.leonardoborges.com/writings/2012/12/02/monads-in-small-bites-part-ii-applicative-functors/
+
 (deftest first-test
 	(testing "first test"
 		(is (= '(2 4 6) (:wrapped (fmap my-list-functor #(* 2 %)))))))
@@ -16,3 +18,16 @@
 (deftest comp-inv-test
 	(testing "composing functions over functors"
 		(is (= '(22 24 26) (:wrapped (fmap my-list-functor (comp g f)))))))
+
+
+(deftest applicative-test
+	(testing "Aplicative functions"
+		(is (= '(2 4 6 11 12 13) (:wrapped (<*> fs xs))))))
+
+(deftest applicative-property
+	(testing "All applicative functions should respect this"
+		(is (= (<*> u (<*> v w))
+					 (-> (pure List (fn [x] (partial comp x)))
+							 (<*> u)
+							 (<*> v)
+							 (<*> w))))))
