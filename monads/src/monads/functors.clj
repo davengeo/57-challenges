d76a8bcd920bea9fe4fde0e3509cff64fe1336ce
David Navarro Alvarez <davengeo@yahoo.es>
2015-09-15 12:18:05 +0300

    adding midjle

--- monads/src/monads/functors.clj
+++ monads/src/monads/functors.clj
@@ -9,4 +9,15 @@
 		(List. (map f (:wrapped functor)))))
 
 (def my-list-functor (List. [1 2 3]))
-<
+
+(fmap my-list-functor #(* 2 %))
+
+(def f #(+ 10 %))
+(def g #(* 2 %))
+
+(fmap my-list-functor (comp f g))
+
+
+
+
+
