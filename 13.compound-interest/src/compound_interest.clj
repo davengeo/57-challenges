c6486fef15f6b4a88cab4be0adfb87a8333aea82
David Navarro Alvarez <davengeo@yahoo.es>
2015-11-11 20:07:55 +0100

    dont work as expected

--- 13.compound-interest/src/compound_interest.clj
+++ 13.compound-interest/src/compound_interest.clj
@@ -1,4 +1,18 @@
- (ns compound-interest)
+(ns compound-interest)
+
 (defn read-bigdec [question]
   (println question)
   (bigdec (read-line)))
+
+(defn expow [x n]
+  (reduce * (repeat n x)))
+
+(defn comp-interest [principal rate period number]
+  (* principal (expow (* number period) (+ 1 (/ rate number)))))
+
+(let [principal (read-bigdec "Enter the principal:")
+      rate (read-bigdec "Enter the rate:")
+      period (read-bigdec "Enter the period:")
+      number (read-bigdec "Enter the number of times the interest is compounded per year:")]
+  (println (comp-interest principal rate period number))
+  (println (expow period number)))
