de29d9575a02c4e546804b90978e4ee4299a04e3
David Navarro Alvarez <davengeo@yahoo.es>
2015-12-24 21:00:20 +0100

    round function

--- 13.compound-interest/src/compound_interest.clj
+++ 13.compound-interest/src/compound_interest.clj
@@ -7,6 +7,12 @@
 (defn expow [x n]
   (reduce * (repeat n x)))
 
+(defn round2
+  "Round a double to the given precision (number of significant digits)"
+  [precision d]
+  (let [factor (Math/pow 10 precision)]
+    (/ (Math/round (* d factor)) factor)))
+
 (defn comp-interest [principal rate period number]
   (* principal (expow (+ 1 (/ (/ rate 100) number)) (* number period))))
 
@@ -14,5 +20,5 @@
       rate (read-bigdec "Enter the rate:")
       period (read-bigdec "Enter the period:")
       number (read-bigdec "Enter the number of times the interest is compounded per year:")]
-  (println (comp-interest principal rate period number))
-  (println (expow period number)))
+  (println (round2 2 (comp-interest principal rate period number))))
+
