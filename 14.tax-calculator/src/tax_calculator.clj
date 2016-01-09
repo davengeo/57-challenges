08c03ae0fb38ceff4a8128cd2565c2caaefdd4a1
David Navarro Alvarez <davengeo@yahoo.es>
2016-01-09 11:58:27 +0100

    solved, needs some details though

--- 14.tax-calculator/src/tax_calculator.clj
+++ 14.tax-calculator/src/tax_calculator.clj
@@ -4,5 +4,15 @@
   (println question)
   (bigdec (read-line)))
 
+(defn read-string [question]
+  (println question)
+  (read-line))
+
 (defn tax [amount state]
-  (if state))
+  (if (= "ws" state)
+    (bigdec (* amount 1, 055))))
+
+(let [amount (read-bigdec "What is the order amount?")
+      state (read-string "What is the state?")]
+  (println (str "The subtotal is $" amount)
+           (println (str "The tax is $" (tax amount state)))))
