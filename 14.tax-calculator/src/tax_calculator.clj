8d4d41a114a5fc600218ce380cffc2ecf6fc89df
David Navarro Alvarez <davengeo@yahoo.es>
2016-01-13 19:57:24 +0100

    round up

--- 14.tax-calculator/src/tax_calculator.clj
+++ 14.tax-calculator/src/tax_calculator.clj
@@ -8,11 +8,16 @@
   (println question)
   (read-line))
 
+(def ^:const TWO_DIGITS 100)
+
+(defn round [amount]
+  (bigdec (/ (Math/ceil (* amount TWO_DIGITS)) TWO_DIGITS)))
+
 (def state-taxes {:WI 0.055 :LI 0.077})
 
 (defn tax [amount state]
   (let [rate (state-taxes (keyword state))]
-    (if (= nil rate) 0 (* amount rate))))
+    (if (= nil rate) 0 (round (* amount rate)))))
 
 (let [amount (read-bigdec "What is the order amount?")
       state (read-string "What is the state?")
