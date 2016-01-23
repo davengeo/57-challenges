446437eb6691674a9364fe0e0effd3f8a5ab29f8
David Navarro Alvarez <davengeo@yahoo.es>
2016-01-23 17:58:55 +0100

    solved with input guard

--- 16.legal-driving-age/src/legal_driving_age.clj
+++ 16.legal-driving-age/src/legal_driving_age.clj
@@ -2,14 +2,18 @@
 
 (defn read-input [question]
   (println question)
-  (bigint (read-line)))
+  (read-line))
+
+(defn to-int-or-nil [chs] (if (re-matches #"[0-9]+" chs) (bigint chs) nil))
 
 (def LEGAL_AGE 16)
 
 (let [age (read-input "What is your age, kid?")]
-  (if (< age LEGAL_AGE)
-    (println "You are not old enough to drive, brat")
-    (println "You are old enough to drive, Kimi")
+  (if-not (to-int-or-nil age)
+    (println "invalid input, brah")
+    (if (< (to-int-or-nil age) LEGAL_AGE)
+      (println "You are not old enough to drive, brat")
+      (println "You are old enough to drive, Kimi"))
     )
   )
 
